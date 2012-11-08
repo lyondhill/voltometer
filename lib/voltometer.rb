52f43389e0dfaf283630e83fd379bb3bd322fcd4
lyon <lyondhill@gmail.com>
2012-11-08 09:45:20 -0600

    more

---
+++ lib/voltometer.rb
@@ -0,0 +1,16 @@
+require "voltometer/version"
+
+module Voltometer
+  class Monitor
+
+
+    def start
+      puts "starting"
+    end    
+
+    def stop
+      puts "stopping"
+    end
+
+  end
+end
---
+++ lib/voltometer/version.rb
@@ -0,0 +1,3 @@
+module Voltometer
+  VERSION = "0.0.1"
+end
