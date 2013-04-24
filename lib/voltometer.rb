48979d9141750223174bd360e82cb848c84d992b
lyon <lyondhill@gmail.com>
2013-04-24 10:52:49 -0600

    try that garbage

--- lib/voltometer.rb
+++ lib/voltometer.rb
@@ -34,7 +34,7 @@ module Voltometer
 
     def start
       log "starting listener"
-      listener.start(false)
+      listener.adaptor.start
       log "listener started"
       self.active = true
       while self.active
