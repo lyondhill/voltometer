f12eb7754a2ea8bca74b43451b2cad05ce4e11c7
lyon <lyondhill@gmail.com>
2013-06-24 16:27:04 -0600

    logg

--- lib/voltometer.rb
+++ lib/voltometer.rb
@@ -100,6 +100,7 @@ module Voltometer
       self.names = names
       insert_report(time, voltage)
       graphite.push_to_graphite do |g|  
+        log "#{names.gsub(/ /, '-').gsub(/\|/, '.')} #{voltage} #{graphite.time_now}"
         g.puts "#{names.gsub(/ /, '-').gsub(/\|/, '.')} #{voltage} #{graphite.time_now}"
       end
     end
